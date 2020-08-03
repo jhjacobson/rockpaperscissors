b9a29a38506e73a54714de8b7cd70e76ab2b7e75
Josh Jacobson <joshhjacobson@gmail.com>
2020-08-02 21:05:47 -0500

    Add basic GUI to play game

---
+++ js/gameplay.js
@@ -0,0 +1,79 @@
+const roundResult = document.querySelector('#roundResult');
+const gameResultPara = document.querySelector('#gameResult');
+const MAXROUNDS = 5;
+let gameResult = [0,0,0,0];
+
+
+const userChoices = document.querySelectorAll('button');
+userChoices.forEach((button) => {
+    button.addEventListener('click', () => {
+        let roundResult = playRound(button.id);
+        gameResult = updateGameResult(roundResult,gameResult);
+
+        if (gameResult[3] == 1) {
+            gameResultPara.textContent = "";
+        }
+
+        if (gameResult[3] == MAXROUNDS) {
+            gameResultPara.textContent = `Final Results: You have ${gameResult[0]} wins, 
+            ${gameResult[1]} losses, and ${gameResult[2]} ties.`
+            gameResult = [0,0,0,0];
+        }
+    });
+});
+
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    let result = getRoundResult(playerSelection,computerSelection);
+    roundResult.textContent = printRoundResult(playerSelection,computerSelection,result);
+    return result;
+}
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    let options = ["Rock","Paper","Scissors"];
+    return options[choice];
+}
+
+function getRoundResult(playerSelection,computerSelection) {
+    if(playerSelection === computerSelection) {
+        return "tie";
+    }
+    else if((playerSelection === "Rock" && computerSelection === "Scissors") ||
+            (playerSelection === "Scissors" && computerSelection === "Paper") ||
+            (playerSelection === "Paper" && computerSelection === "Rock")){
+                return "win";
+            }
+    else {
+        return "lose";
+    }   
+}
+
+function printRoundResult(playerSelection,computerSelection,result) {
+    switch(result) {
+        case "win":
+            return `You Win! ${playerSelection} beats ${computerSelection}.`;
+            break;
+        case "lose":
+            return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+            break;
+        case "tie":
+            return `You tie! You both chose ${playerSelection}.`;
+    }
+
+}
+
+function updateGameResult(roundResult,gameResult) {
+    switch(roundResult) {
+        case "win":
+            gameResult[0]+=1;
+            break;
+        case "lose":
+            gameResult[1]+=1;
+            break;
+        case "tie":
+            gameResult[2]+=1;
+    }
+    gameResult[3]+=1;
+    return gameResult;
+}
